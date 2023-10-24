b6925a31056c0a7c0468d9a6a8b67f1fbc6fa9b1
Gusta <gustavorodrigues182@live.com>
2023-10-24 05:54:16 -0300

    Feat: AlunoSerializer2

--- escola/serializer.py
+++ escola/serializer.py
@@ -30,3 +30,10 @@ class ListaAlunosMatriculadosSerializer(serializers.ModelSerializer):
     class Meta:
         model = Matricula
         fields = ['aluno_nome']
+
+### API V2:
+
+class AlunoSerializerV2(serializers.ModelSerializer):
+    class Meta:
+        model = Aluno
+        fields = ['id', 'nome', 'celular' 'rg', 'cpf', 'data_nascimento']

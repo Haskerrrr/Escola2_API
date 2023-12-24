4271202f62f1387f9d19df30e76f44b2200b7ff3
Gusta <gustavorodrigues182@live.com>
2023-12-24 04:35:47 -0300

    Feat: Restrição por tentativas

--- setup/settings.py
+++ setup/settings.py
@@ -123,19 +123,19 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.QueryParameterVersioning',
-    'DEFAULT_PERMISSION_CLASSES':[
-        'rest_framework.permissions.IsAuthenticated', 
-        'rest_framework.permissions.DjangoModelPermissions'
-    ], 
+    # 'DEFAULT_PERMISSION_CLASSES':[
+    #     'rest_framework.permissions.IsAuthenticated', 
+    #     'rest_framework.permissions.DjangoModelPermissions'
+    # ], 
     'DEFAULT_AUTHENTICATION_CLASSES':[
         'rest_framework.authentication.BasicAuthentication'
     ], 
     'DEFAULT_THROTTLE_CLASSES': [
-        'rest_framework.throttling.AnonRateThrottle',
-        'rest_framework.throttling.UserRateThrottle'
+        'rest_framework.throttling.AnonRateThrottle', #Usuário Anonimo
+        # 'rest_framework.throttling.UserRateThrottle' #Qualquer usuário
     ],
     'DEFAULT_THROTTLE_RATES': {
-        'anon': '100/day',
-        'user': '1000/day'
+        'anon': '5/day',
+        # 'user': '1000/day'
     }
 }
